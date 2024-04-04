bbe912c9da661182baabea9fdbf76c18ac53141b
isabellamo <142440827+isabellamo@users.noreply.github.com>
2024-04-04 11:46:27 -0700

    Update WordList.java

--- src/main/java/WordList.java
+++ src/main/java/WordList.java
@@ -1,28 +1,35 @@
 import java.util.*;
 
-public class WordList
-{
+public class WordList {
   private ArrayList <String> myList;
 
-  public WordList() { myList = new ArrayList<String>(); }
-  public void add(String word) { myList.add(word); }
-  public String toString() { return myList.toString(); }
+  public WordList() { 
+    myList = new ArrayList<String>(); 
+  }
+  
+  public void add(String word) { 
+    myList.add(word); 
+  }
+  
+  public String toString() { 
+    return myList.toString(); 
+  }
 
   public int numWordsOfLength(int len){
     /* to be implemented in part (a) */
-    int num = 0;
+    int count = 0;
     
     for (int i = 0; i < myList.size(); i++){
       if (myList.get(i).length() == len){
-        num++;
+        count++;
       }
     }
-    return num;
+    return count;
   }
 
   public void removeWordsOfLength(int len){
     /* to be implemented in part (b) */
-    for (int i =myList.size() - 1; i >= 0; i--){
+    for (int i = myList.size() - 1; i >= 0; i--){
       if (myList.get(i).length() == len){
         myList.remove(i);
       }
